d8e7bac6001e5922eee27a6b5d4a7c941364494f
siwhelan <simon@swhelan.dev>
2024-02-25 17:26:46 +0000

    Add React Router and related dependencies. Updated Tailwind config

---
+++ frontend/src/pages/TestPage.jsx
@@ -0,0 +1,13 @@
+import Button from "../components/button";
+
+const TestPage = () => {
+  return (
+    <div>
+      <h1>This is a Test Page</h1>
+      <p>Click the button below:</p>
+      <Button />
+    </div>
+  );
+};
+
+export default TestPage;
